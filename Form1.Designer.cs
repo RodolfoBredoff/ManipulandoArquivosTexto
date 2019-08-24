67683a8462120d3e4cda7356ad5c2c31b8bc4942
RodolfoSoares <rodolfobredoff@gmail.com>
2019-08-24 14:31:04 -0300

    Manipulando Arquivos Texto

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ManipulandoArquivosTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCriar = new System.Windows.Forms.Button();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnConcatenar = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCriar
+            // 
+            this.btnCriar.Location = new System.Drawing.Point(139, 80);
+            this.btnCriar.Name = "btnCriar";
+            this.btnCriar.Size = new System.Drawing.Size(189, 33);
+            this.btnCriar.TabIndex = 0;
+            this.btnCriar.Text = "Criar arquivo";
+            this.btnCriar.UseVisualStyleBackColor = true;
+            this.btnCriar.Click += new System.EventHandler(this.BtnCriar_Click);
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(139, 119);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(189, 33);
+            this.btnAbrir.TabIndex = 1;
+            this.btnAbrir.Text = "Abrir arquivo";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.BtnAbrir_Click);
+            // 
+            // btnConcatenar
+            // 
+            this.btnConcatenar.Location = new System.Drawing.Point(139, 158);
+            this.btnConcatenar.Name = "btnConcatenar";
+            this.btnConcatenar.Size = new System.Drawing.Size(189, 33);
+            this.btnConcatenar.TabIndex = 2;
+            this.btnConcatenar.Text = "Concatenar arquivo";
+            this.btnConcatenar.UseVisualStyleBackColor = true;
+            this.btnConcatenar.Click += new System.EventHandler(this.BtnConcatenar_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(139, 197);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(189, 33);
+            this.btnAlterar.TabIndex = 3;
+            this.btnAlterar.Text = "Alterar arquivo";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.BtnAlterar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(139, 236);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(189, 33);
+            this.btnExcluir.TabIndex = 4;
+            this.btnExcluir.Text = "Excluir arquivo";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.BtnExcluir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 450);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnConcatenar);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.btnCriar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCriar;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Button btnConcatenar;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnExcluir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManipulandoArquivosTexto
+{
+    public partial class Form1 : Form
+    {
+
+        private string strPathFile = @"C:\temp\ArquivoExemplo.txt";
+
+        private void Criar()
+        {
+            try
+            {
+                //Usarei a cláusula using como boas práticas de programação em todos os métodos
+                //Instancio a classe FileStream, uso a classe File e o método Create para criar o
+                //arquivo passando como parâmetro a variável strPathFile, que contém o arquivo
+                using (FileStream fs = File.Create(strPathFile))
+                {
+                    //Crio outro using, dentro dele instancio o StreamWriter (classe para gravar os dados)
+                    //que recebe como parâmetro a variável fs, referente ao FileStream criado anteriormente
+                    using (StreamWriter sw = new StreamWriter(fs))
+                    {
+                        //Uso o método Write para escrever algo em nosso arquivo texto
+                        sw.Write("Texto adicionado ao exemplo!");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            //Se tudo ocorrer bem, exibo a mensagem ao usuário.
+            MessageBox.Show("Arquivo criado com sucesso!!!");
+        }
+
+        private void Abrir()
+        {
+            try
+            {
+                //Verifico se o arquivo que desejo abrir existe e passo como parâmetro a respectiva variável
+                if (File.Exists(strPathFile))
+                {
+                    //Se existir "starto" um processo do sistema para abrir o arquivo e, sem precisar
+                    //passar ao processo o aplicativo a ser aberto, ele abre automaticamente o Notepad
+                    System.Diagnostics.Process.Start(strPathFile);
+                }
+                else
+                {
+                    //Se não existir exibo a mensagem
+                    MessageBox.Show("Arquivo não encontrado!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Concatenar()
+        {
+            try
+            {
+                // Verifico se o arquivo que desejo
+                // abrir existe e passo como parâmetro a
+                // respectiva variável
+                if (File.Exists(strPathFile))
+                {
+                    // Crio um using, dentro dele instancio
+                    // StreamWriter, uso a classe File e o
+                    // método AppendText para concatenar o
+                    // texto, passando como parâmetro a
+                    // variável strPathFile
+                    using (StreamWriter sw =
+                 File.AppendText(strPathFile))
+                    {
+                        // Uso o método Write para escrever o arquivo
+                        // que será adicionado no arquivo text
+                        sw.Write("\r\nTexto adicionado ao final do arquivo");
+                    }
+                    // Exibo a mensagem que o arquivo foi atualizado
+                 MessageBox.Show("Arquivo atualizado!");
+                }
+                else
+                {
+                    // Se não existir exibo a mensagem
+                    MessageBox.Show("Arquivo não encontrado!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void Alterar()
+        {
+            try
+            {
+                // Verifico se existe a pasta
+                //arquivo passando como parâmetro a variável strPathFile, que contém o arquivo
+                if (File.Exists(strPathFile))
+                {
+
+                    using (StreamWriter sw = new StreamWriter(strPathFile))
+                    {
+                        //Uso o método Write para sobescrever algo em nosso arquivo texto
+                        sw.Write("OUTRO Texto atualizado ao exemplo!");
+                    }
+                }
+                else
+                {
+                    // Se não existir exibo a mensagem
+                    MessageBox.Show("Arquivo não encontrado!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Excluir()
+        {
+            try
+            {
+                // Verificando se existe arquivo na pasta
+                if (File.Exists(strPathFile))
+                {
+                    // Excluindo arquivo caso ele exista
+                    File.Delete(strPathFile);
+                    
+                }
+                else
+                {
+                    // Se não existir exibo a mensagem
+                    MessageBox.Show("Arquivo não encontrado!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCriar_Click(object sender, EventArgs e)
+        {
+            Criar();
+        }
+
+        private void BtnAbrir_Click(object sender, EventArgs e)
+        {
+            Abrir();
+        }
+
+        private void BtnConcatenar_Click(object sender, EventArgs e)
+        {
+            Concatenar();
+        }
+
+        private void BtnAlterar_Click(object sender, EventArgs e)
+        {
+            Alterar();
+        }
+
+        private void BtnExcluir_Click(object sender, EventArgs e)
+        {
+            Excluir();
+        }
+    }
+}
